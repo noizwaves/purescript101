0ad4645ca7899c751da9ad5b3766a748c1fae5ea
Adam Neumann <noizwaves@users.noreply.github.com>
2019-07-28 15:57:24 -0600

    Finish 2.10 Computing Diagonals

--- src/Main.purs
+++ src/Main.purs
@@ -1,5 +1,11 @@
 module Main where
 
-import Effect.Console (log)
+import Prelude
 
-main = log "Hello, World!"
+import Effect.Console (logShow)
+import Math (sqrt)
+
+
+diagonal w h = sqrt (w * w + h * h)
+
+main = logShow (diagonal 3.0 4.0)
